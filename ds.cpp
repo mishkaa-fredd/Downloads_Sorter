ee5eaebd3491be42353a8f5572101e33f2312116
mishkaa-fredd <misha2014ns@gmail.com>
2026-03-12 21:37:35 +0200

    Add files via upload

---
+++ ds.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[]) 
+{
+    string downloads;
+
+    if (argc > 1) 
+    {
+        downloads = argv[1];
+    } else 
+    {
+    cout << "Enter the path to your downloads folder: ";
+    cin >> downloads;
+    }
+
+    for (const auto& file : fs::directory_iterator(downloads)) 
+    {
+        string ext = file.path().extension().string();
+        if (ext == ".jpg" || ext == ".png" || ext == ".gif" || ext == ".svg")
+        {
+            fs::create_directory(downloads + "/Images");
+            fs::rename(file.path(), downloads + "/Images/" + file.path().filename().string());
+        } else if (ext == ".mp4" || ext == ".mov" || ext == ".webm")
+        {
+            fs::create_directory(downloads + "/Videos");
+            fs::rename(file.path(), downloads + "/Videos/" + file.path().filename().string());
+        } else if (ext == ".pdf" || ext == ".docx" || ext == ".doc" || ext == ".txt" || ext == ".xlsx" || ext == ".xls" || ext == ".pptx" || ext == ".ppt"  )
+        {
+            fs::create_directory(downloads + "/Documents");
+            fs::rename(file.path(), downloads + "/Documents/" + file.path().filename().string());     
+        } else if (ext == ".zip" || ext == ".rar" || ext == ".7z") 
+        {
+            fs::create_directory(downloads + "/Archives");
+            fs::rename(file.path(), downloads + "/Archives/" + file.path().filename().string());
+        } else if (ext == ".exe" || ext == ".msi") 
+        {
+            fs::create_directory(downloads + "/Programs");
+            fs::rename(file.path(), downloads + "/Programs/" + file.path().filename().string());
+        } else if (ext == ".mp3" || ext == ".wav") 
+        {
+            fs::create_directory(downloads + "/Music");
+            fs::rename(file.path(), downloads + "/Music/" + file.path().filename().string());
+        } else if (ext == ".py" || ext == ".js" || ext == ".ts" || ext == ".java" || ext == ".cs" || ext == ".cpp" || ext == ".h" || ext == ".html" || ext == ".css") 
+        {
+            fs::create_directory(downloads + "/Code");
+            fs::rename(file.path(), downloads + "/Code/" + file.path().filename().string());
+        } else if (ext == ".iso" || ext == ".dmg") 
+        {
+            fs::create_directory(downloads + "/Disk Images");
+            fs::rename(file.path(), downloads + "/Disk Images/" + file.path().filename().string());
+        } else if (ext == ".torrent") 
+        {
+            fs::create_directory(downloads + "/Torrents");
+            fs::rename(file.path(), downloads + "/Torrents/" + file.path().filename().string());
+        } else if (ext == ".otf" || ext == ".ttf") 
+        {
+            fs::create_directory(downloads + "/Fonts");
+            fs::rename(file.path(), downloads + "/Fonts/" + file.path().filename().string());
+        }
+    }
+    return 0;
+} 
